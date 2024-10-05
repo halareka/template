4a8c6fcd997f7b2035d1898d199b1098d66ba123
halareka <you@example.com>
2024-10-06 02:27:36 +0300

    feat: Added weekly stat and fix inline-buttons

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 require("dotenv").config();
-
 const TelegramBot = require('node-telegram-bot-api');
 const API_KEY_BOT = process.env.API;
 const bot = new TelegramBot(API_KEY_BOT, {
@@ -21,7 +20,6 @@ const USERS = //стата за день | стата за лучший день
     "user4": "",
     "user5": "",
 }
-
 async function fetchDailyData(url) {
     try {
         const response = await fetch(url);
@@ -30,7 +28,7 @@ async function fetchDailyData(url) {
         }
         const data = await response.json();
         if (data && data.data) {
-            DAY_DATA = data.data[6]; //вытаскиваем стату за день
+            DAY_DATA = data.data; //вытаскиваем стату за день
             return DAY_DATA;
         } else {
             console.error('Data property is missing in the response:', data);
@@ -52,23 +50,32 @@ async function fetchBestData(url) {
         console.error('There has been a problem with your fetch operation:', error);
     }
 }
+function calculateTotalTime(data) {
+    let totalHours = 0;
+    let totalMinutes = 0;
+    data.forEach(entry => {
+      totalHours += entry.grand_total.hours;
+      totalMinutes += entry.grand_total.minutes;
+    });
+    totalHours += Math.floor(totalMinutes / 60);
+    totalMinutes = totalMinutes % 60;
+    return `${totalHours} hrs ${totalMinutes} mins`;
+}
 bot.onText("/stats", (msg) => {
     const chatId = msg.chat.id;
-    // инлайн-кнопки
+    // Inline buttons arranged in multiple rows
     const options = {
         reply_markup: {
             inline_keyboard: [
-                [
-                    { text: 'halareka', callback_data: '1' },
-                    { text: 'user2', callback_data: '2' },
-                    { text: 'user3', callback_data: '3' },
-                    { text: 'user4', callback_data: '4' },
-                    { text: 'user5', callback_data: '5' }
-                ]
+                [{ text: 'halareka', callback_data: '1' },],
+                [{ text: 'user2', callback_data: '2' },],
+                [{ text: 'user3', callback_data: '3' },],
+                [{ text: 'user4', callback_data: '4' },],
+                [{ text: 'user5', callback_data: '5' }]
             ]
         }
     };
-    bot.sendMessage(chatId, 'Выберите пользователя для просмотра дневной статистики:', options);
+    bot.sendMessage(chatId, 'Выберите пользователя для просмотра статистики:', options);
 });
 bot.on('callback_query', async (callbackQuery) => { 
     const chatId = callbackQuery.message.chat.id;
@@ -81,7 +88,7 @@ bot.on('callback_query', async (callbackQuery) => {
             firstUserName = Object.keys(USERS)[userSelection-1]; // Ник-нейм
             data = await fetchDailyData(USERS.halareka[0]); 
             best_data = await fetchBestData(USERS.halareka[1]); 
-            responseMessage = `Статистика <b>${firstUserName}</b> за день \nДата: ${JSON.stringify(DAY_DATA.range.date)},\nВремя кодинга: ${JSON.stringify(DAY_DATA.grand_total.text)}\n——————————————————\n\n\nСтатистика <b>${firstUserName}</b> за лучший день \nДата: ${JSON.stringify(BEST_DAY_DATA.data.best_day.date)},\nВремя кодинга: ${JSON.stringify(BEST_DAY_DATA.data.best_day.text)}\n——————————————————\n\n\nВсего времени: ${JSON.stringify(BEST_DAY_DATA.data.grand_total.human_readable_total)}`
+            responseMessage = `Statistics for <b>${firstUserName}</b> for the day \nDate: ${JSON.stringify(DAY_DATA[6].range.date)},\nCoding time: ${JSON.stringify(DAY_DATA[6].grand_total.text)}\n——————————————————\n\nStatistics for <b>${firstUserName}</b> for the best day \nDate: ${JSON.stringify(BEST_DAY_DATA.data.best_day.date)},\nCoding time: ${JSON.stringify(BEST_DAY_DATA.data.best_day.text)}\n——————————————————\n\nTotal time: ${JSON.stringify(BEST_DAY_DATA.data.grand_total.human_readable_total)}\n\nWeekly statistics: ${calculateTotalTime(DAY_DATA)}`;
             DAY_DATA = {}; BEST_DAY_DATA = {};
             break;
         case '2':
@@ -99,9 +106,7 @@ bot.on('callback_query', async (callbackQuery) => {
         default:
             responseMessage = 'Invalid selection';
     }
-
     bot.sendMessage(chatId, responseMessage, { parse_mode: 'HTML' });
-
     // удаление выбора.
     bot.deleteMessage(chatId, messageId).catch(error => {
         console.error("Failed to delete message:", error);
@@ -109,56 +114,3 @@ bot.on('callback_query', async (callbackQuery) => {
 });
 
 
-bot.onText("/week_stats", (msg) => {
-    const chatId = msg.chat.id;
-    // инлайн-кнопки
-    const options = {
-        reply_markup: {
-            inline_keyboard: [
-                [
-                    { text: 'halareka', callback_data: '1' },
-                    { text: 'user2', callback_data: '2' },
-                    { text: 'user3', callback_data: '3' },
-                    { text: 'user4', callback_data: '4' },
-                    { text: 'user5', callback_data: '5' }
-                ]
-            ]
-        }
-    };
-    bot.sendMessage(chatId, 'Выберите пользователя для просмотра недельной статистики:', options);
-});
-bot.on('callback_query', async (callbackQuery) => { 
-    
-    const chatId = callbackQuery.message.chat.id;
-    const messageId = callbackQuery.message.message_id; 
-    const userSelection = callbackQuery.data;
-    let firstUserName = 0 , data = 0 ;
-    let responseMessage;
-    switch (userSelection) {
-        case '1':            
-            firstUserName = Object.keys(USERS)[userSelection-1]; // Ник-нейм
-            data = await fetchDailyData(USERS.halareka[0]); 
-            break;
-        case '2':
-            responseMessage = 'Извините,данный юзер еще не добавлен:(';
-            break;
-        case '3':
-            responseMessage = 'Извините,данный юзер еще не добавлен:(';
-            break;
-        case '4':
-            responseMessage = 'Извините,данный юзер еще не добавлен:(';
-            break;
-        case '5':
-            responseMessage = 'Извините,данный юзер еще не добавлен:(';
-            break;
-        default:
-            responseMessage = 'Invalid selection';
-    }
-
-    bot.sendMessage(chatId, responseMessage, { parse_mode: 'HTML' });
-
-    // удаление выбора.
-    bot.deleteMessage(chatId, messageId).catch(error => {
-        console.error("Failed to delete message:", error);
-    });
-});
